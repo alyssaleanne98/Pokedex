3a4dbde34dfd8fe17410751df56f7ff18acd0f36
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-30 21:10:04 -0700

    working show route

--- server.js
+++ server.js
@@ -1,40 +1,63 @@
 // Dependencies 
 const express = require('express');
-const app = express()
-// const methodOverride = require('method-override');
-// const methodOverride = require('method-override');
+const app = express();
+const methodOverride = require('method-override');
 
 // Config
 const port = 3000
 
 
 // Middleware
-app.use(express.urlencoded(({extended:false})))
-// app.use(methodOverride('_method'));
+app.use(express.urlencoded(({ extended: false })))
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 
 
 // Database
-const pokemon = require('./models/pokemon.js');
+const Pokemon = require('./models/pokemon.js');
 
 // I N D U C E
 
 // INDEX
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
-// res.render('index.ejs', { 
-//     allPokemon: pokemon });
-});
+    res.send(Pokemon)
+    // res.render('index.ejs', { 
+    //     allPokemon: Pokemon, 
+    })
+// })
+
+//NEW
+// app.get('pokemon/new', (req, res) => {
+//     res.send("this is the new route")
+// });
 
 
 // // SHOW
-// app.get('/:id', (req, res) => {
-// res.render('show.ejs', { data: Pokemon[req.params.id] });
+//what is req.params.id? this is an object containing the parameter values parsed from the URL path. the "name" from the url path will be available when you use req.params.name.
+app.get('/pokemon/:id', (req, res) => {
+    console.log(req.params)
+    console.log(Pokemon[req.params.id])
+    res.render('show.ejs', {
+        allPokemon: Pokemon[req.params.id],
+    });
+});
+
+
+
+// //CREATE 
+// app.post('/pokemon', (req, res) => {
+//     res.send("this is the create route")
 // });
+
+// //EDIT
+// app.get('/pokemon/:id/edit', (req, res) => {
+//     res.send("this is the edit route")
+// });
+
 
 // Listener 
 app.listen(port, () => {
-	console.log(`listening to pokedex on port ${port}`)
+    console.log(`listening to pokedex on port ${port}`)
 })
 
